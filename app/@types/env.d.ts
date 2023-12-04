51c888ea6bf698147d282e439712953eb2c9f28b
nekelpatrick <nekelpatrick@gmail.com>
2023-12-04 19:51:35 -0300

    add missing env variable

--- app/@types/env.d.ts
+++ app/@types/env.d.ts
@@ -1,7 +1,8 @@
 declare module "@env" {
-  export const EXPO_PUBLIC_ANDROID_CLIENT_ID: string;
+  export const ANDROID_CLIENT_ID: string;
   export const EXPO_PUBLIC_IOS_CLIENT_ID: string;
   export const EXPO_PUBLIC_REALM_APP_ID: string;
   export const EXPO_PUBLIC_GOOGLE_MAPS_API_KEY: string;
   export const EXPO_G_CLOUD_APP_NAME: string;
+  export const EXPO_PROXY_CLIENT_ID: string;
 }
