7543416dd1b147e0ff91d95d0f5e57efc48411f7
Mariana Antunes <marimsantunes@gmail.com>
2023-12-27 13:52:20 -0300

    feat: add schemas to application

--- app/libs/realm/index.ts
+++ app/libs/realm/index.ts
@@ -1,9 +1,16 @@
 import { createRealmContext } from "@realm/react";
 
+import { Car } from "./schemas/car";
 import { Historic } from "./schemas/historic";
+import { PassengerRoute } from "./schemas/passenger_route";
+import { Preferences } from "./schemas/preferences";
+import { Tipping } from "./schemas/tipping";
 import { User } from "./schemas/user";
+import { Ride } from "./schemas/ride";
+
+export const schemas = [Car, Historic, PassengerRoute, Preferences, Ride, Tipping, User]
 
 export const { RealmProvider, useRealm, useQuery, useObject } =
   createRealmContext({
-    schema: [Historic, User],
+    schema: schemas,
   });
